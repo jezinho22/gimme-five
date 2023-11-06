bae612569553b6f7066fdc0bdde56b59a09673c8
Jeremy Johns <jez.johns@gmail.com>
2023-11-06 08:18:57 +0000

    added data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import "./reset.css";
 import "./App.css";
 import Header from "./components/Header";
+import Arrays from "./components/Arrays";
 import TestOne from "./components/TestOne";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<TestOne />
+			<Arrays />
 		</div>
 	);
 }
