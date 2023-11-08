18e740a106c3531a6598a094dd8d1fe56949bb6f
Jeremy Johns <jez.johns@gmail.com>
2023-11-08 19:53:52 +0000

    trying repl

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import Header from "./components/Header";
 import Arrays from "./components/Arrays";
 import TestOne from "./components/TestOne";
+import Replit from "./components/Replit";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<Arrays />
+			<Replit />
 		</div>
 	);
 }
---
+++ client/src/components/Replit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Replit() {
+	return (
+		<div>
+			<iframe
+				title="replit-one"
+				src="https://replit.com/@JezJohns/Object-and-array-copying"
+				width="600"
+				height="400"
+			/>
+		</div>
+	);
+}
